671ec333ed99ee4ac0bf8a38edb44d5cc61fc461
KARMVEER SINGH SARWARA <99719057+karmveersarwara@users.noreply.github.com>
2023-02-24 21:27:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const openai = new OpenAIApi(configuration);
 
 // app.route('/chat', methods=['POST']);
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
